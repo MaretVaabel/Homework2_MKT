b395befd998b1ad4c1a87e20789aa4b3f42adcde
Kristiina Köster <56896621+kristiinakoster@users.noreply.github.com>
2020-10-24 22:18:12 +0300

    edited javascript to display photos and images

--- res/js/script.js
+++ res/js/script.js
@@ -1,3 +1,20 @@
+
+
+function loadPosts() {
+    return $.get(
+        {
+            url: 'https://private-anon-e0d4b76204-wad20postit.apiary-mock.com/posts',
+            success: function (response) {
+                return response;
+            },
+            error: function () {
+                alert('error')
+            }
+        }
+    );
+}
+
+
 $(function () {
 
 
@@ -12,43 +29,48 @@ $(function () {
         alert('Error loading user info')
     });
 
-
-
+    /*$(".like-button").supertoggle({
+        onVal: "liked",
+        onContent: "liked",
+        offVal: "disliked",
+        offContent: "disliked",
+    });*/
 
 });
 
 
 function displayPost(posts) {
     for (let i = 0; i < posts.length; i++) {
-        const $media = $('<div>');
+        let media = $('<div>'); 
+        let media_media = $('<div>');
         if(posts[i].media != null){
-            if(posts[i].media.type == 'video'){
-             const $media = $('<video>').attr('video', posts[i].media.url);
+            if(posts[i].media.type =='video'){    
+            media_media = $('<video>').attr('src', posts[i].media.url); // kas siin viga?
             }
-            const $media = $('<img>').attr('src', posts[i].media.url);
-        }
-        const $avatar = $('<img>').attr('src', posts[i].author.avatar).attr('alt', 'Post autor');
-      
-      
+            else {
+            media = $('<img>').attr('src', posts[i].media.url);// kas siin viga?
+            }
+        
+        $('.post-avatar').attr('src', posts[i].author.avatar).attr('alt', 'Post autor');
+        
         $('.main-container').append(`
         <div class="post">
             <div class="post-author">
                 <span class="post-author-info">
-                    ${$avatar}
+                    <img src="res/images/avatar.png" class="post-avatar" alt="Post author">
                     <small>${ posts[i].author.firstname + " " + posts[i].author.lastname } </small>
                 </span>
                 <small>${posts[i].createTime}</small>
             </div>
             <div class="post-image">
-          ${$media}
+                ${media}
             </div>
-
             <div class="post-title">
                 <h3>${posts[i].text}</h3>
             </div>
 
             <div class="post-actions">
-                <button type="button" name="like" class="like-button">${posts[i].likes}</button>
+                <button type="button" name="like" class="like-button">${posts[i].likes}</button> 
              </div>
         </div>`);
 
@@ -57,16 +79,8 @@ function displayPost(posts) {
     }
 }
 
-function loadPosts() {
-    return $.get(
-        {
-            url: 'https://private-anon-e0d4b76204-wad20postit.apiary-mock.com/posts',
-            success: function (response) {
-                return response;
-            },
-            error: function () {
-                alert('error')
-            }
-        }
-    );
+
+
+
+
 }
