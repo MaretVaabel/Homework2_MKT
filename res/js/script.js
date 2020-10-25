82a811a711392e70700d317ed3e0d7729d2d1a88
Kristiina Köster <56896621+kristiinakoster@users.noreply.github.com>
2020-10-25 22:15:14 +0200

    worked on script file

--- res/js/script.js
+++ res/js/script.js
@@ -1,3 +1,59 @@
+$(function () {
+    loadPosts()
+    .then(function (postResponse) {
+        console.log("Post response: ", postResponse)
+        posts = postResponse;
+        displayPost(posts);
+        
+        $(".like-button").click( function() {
+            var likes = $(this).text()
+
+            var count = countLikes(likes) + 1
+            //console.log(count)
+            $(this).toggleClass("liked");
+        });
+    })
+    .catch(function () {
+        alert('Error loading user info')
+    });
+
+});
+
+function countLikes (text){
+    var textLeft = text.split('k')[0]
+    var number = parseInt(textLeft);
+    return number*1000
+}
+
+
+function displayPost(posts) {
+    for (let i = 0; i < posts.length; i++) {
+
+        $('.main-container').append(`
+        <div class="post">
+            <div class="post-author">
+                <span class="post-author-info">
+                    <img src="${posts[i].author.avatar}" alt="Post author">
+                    <small>${ posts[i].author.firstname + " " + posts[i].author.lastname } </small>
+                </span>
+                <small>${posts[i].createTime}</small>
+            </div>
+            <div class="post-image"> 
+                ${posts[i].media != null ? (posts[i].media.type == 'video' ? '<video controls><source src="'+posts[i].media.url+'" class="post-video" type="video/mp4"></video>' :  '<img src="'+posts[i].media.url+'" class="image" alt=""></img>') : ' '}
+            </div>
+
+            <div class="post-title">
+                <h3>${posts[i].text == null ? '' : posts[i].text}</h3>
+            </div>
+
+            <div class="post-actions">
+                <button type="button" name="like" class="like-button">${posts[i].likes}</button>
+             </div>
+        </div>`)
+        
+
+    }
+}
 
 
 function loadPosts() {
@@ -12,75 +68,4 @@ function loadPosts() {
             }
         }
     );
-}
-
-
-$(function () {
-
-
-
-    loadPosts()
-    .then(function (postResponse) {
-        console.log("Post response: ", postResponse)
-        posts = postResponse;
-        displayPost(posts);
-    })
-    .catch(function () {
-        alert('Error loading user info')
-    });
-
-    /*$(".like-button").supertoggle({
-        onVal: "liked",
-        onContent: "liked",
-        offVal: "disliked",
-        offContent: "disliked",
-    });*/
-
-});
-
-
-function displayPost(posts) {
-    for (let i = 0; i < posts.length; i++) {
-        let media = $('<div>'); 
-        let media_media = $('<div>');
-        if(posts[i].media != null){
-            if(posts[i].media.type =='video'){    
-            media_media = $('<video>').attr('src', posts[i].media.url); // kas siin viga?
-            }
-            else {
-            media = $('<img>').attr('src', posts[i].media.url);// kas siin viga?
-            }
-        
-        $('.post-avatar').attr('src', posts[i].author.avatar).attr('alt', 'Post autor');
-        
-        $('.main-container').append(`
-        <div class="post">
-            <div class="post-author">
-                <span class="post-author-info">
-                    <img src="res/images/avatar.png" class="post-avatar" alt="Post author">
-                    <small>${ posts[i].author.firstname + " " + posts[i].author.lastname } </small>
-                </span>
-                <small>${posts[i].createTime}</small>
-            </div>
-            <div class="post-image">
-                ${media}
-            </div>
-            <div class="post-title">
-                <h3>${posts[i].text}</h3>
-            </div>
-
-            <div class="post-actions">
-                <button type="button" name="like" class="like-button">${posts[i].likes}</button> 
-             </div>
-        </div>`);
-
-        
-
-    }
-}
-
-
-
-
-
 }
